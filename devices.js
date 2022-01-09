f90ca9aae272848b8028273f57728d8d039963d2
Jack Cannon <jackc@annon.co.uk>
2022-01-09 01:58:54 +0000

    Add config setting for dither, and consolidate 1bit convert functions

--- devices.js
+++ devices.js
@@ -1,4 +1,4 @@
-const image = require('./image.js');
+const image = require('./image');
 const waveshare4In2Driver = require('bindings')('waveshare4in2.node');
 const waveshare7in5v2Driver = require('bindings')('waveshare7in5v2.node');
 const waveshare3In7Driver = require('bindings')('waveshare3in7.node');
@@ -9,298 +9,249 @@ const waveshare2in7bDriver = require('bindings')('waveshare2in7b.node');
 const waveshare2in7v2Driver = require('bindings')('waveshare2in7v2.node');
 const waveshare2in7v2bDriver = require('bindings')('waveshare2in7bv2.node');
 
-const waveshare4in2Horizontal = {
-    height: 300,
-    width: 400,
-    driver: waveshare4In2Driver,
-    displayPNG: async function (imgContents) {
-        const buffer = await image.convertPNGto1BitBW(imgContents);
-        this.driver.display(buffer);
+const defaultDevice = {
+    height: 0, // number - height resolution of device
+    width: 0, // number - width resolution of device
+    rotated: false, // boolean - is device rotated 90 degrees
+    rightToLeft: false, // boolean - does device buffer read right to left
+    numColors: 2, // number - 2 (1bitBW) or 4 (4gray)
+    driver: undefined, // binding
+    displayPNG: async function (imgContents, dither) {
+        const args = [imgContents, this.rotated, dither, this.rightToLeft];
+
+        if (this.numColors === 4) {
+            const buffer = await image.convertPNGto4Grey(...args);
+            this.driver.display_4GrayDisplay(buffer);
+        } else {
+            const buffer = await image.convertPNGto1BitBW(...args);
+            this.driver.display(buffer);
+        }
     },
     init: function () {
-        this.driver.init();
+        if (this.numColors === 4) {
+            this.driver.init_4Gray();
+        } else {
+            this.driver.init();
+        }
     },
+};
+
+const waveshare4in2Horizontal = {
+    ...defaultDevice,
+    height: 300,
+    width: 400,
+    rotated: false,
+    rightToLeft: false,
+    numColors: 2,
+    driver: waveshare4In2Driver,
 };
 
 const waveshare4in2Vertical = {
+    ...defaultDevice,
     height: 400,
     width: 300,
+    rotated: true,
+    rightToLeft: false,
+    numColors: 2,
     driver: waveshare4In2Driver,
-    displayPNG: async function (imgContents) {
-        const buffer = await image.convertPNGto1BitBWRotated(imgContents);
-        this.driver.display(buffer);
-    },
-    init: function () {
-        this.driver.init();
-    },
 };
 
 const waveshare4in2HorizontalGray = {
+    ...defaultDevice,
     height: 300,
     width: 400,
+    rotated: false,
+    rightToLeft: false,
+    numColors: 4,
     driver: waveshare4In2Driver,
-    displayPNG: async function (imgContents) {
-        const buffer = await image.convertPNGto4Grey(imgContents);
-        this.driver.display_4GrayDisplay(buffer);
-    },
-    init: function () {
-        this.driver.init_4Gray();
-    },
 };
 
 const waveshare4in2VerticalGray = {
+    ...defaultDevice,
     height: 400,
     width: 300,
+    rotated: true,
+    rightToLeft: false,
+    numColors: 4,
     driver: waveshare4In2Driver,
-    displayPNG: async function (imgContents, color_depth) {
-        const buffer = await image.convertPNGto4GreyRotated(imgContents);
-        this.driver.display_4GrayDisplay(buffer);
-    },
-    init: function () {
-        this.driver.init_4Gray();
-    },
 };
 
 const waveshare7in5v2Horizontal = {
+    ...defaultDevice,
     height: 480,
     width: 800,
+    rotated: false,
+    rightToLeft: false,
+    numColors: 2,
     driver: waveshare7in5v2Driver,
-    displayPNG: async function (imgContents) {
-        const buffer = await image.convertPNGto1BitBW(imgContents);
-        this.driver.display(buffer);
-    },
-    init: function () {
-        this.driver.init();
-    },
 };
 
-const waveshare7in2v2Vertical = {
+const waveshare7in5v2Vertical = {
+    ...defaultDevice,
     height: 800,
     width: 480,
+    rotated: true,
+    rightToLeft: false,
+    numColors: 2,
     driver: waveshare7in5v2Driver,
-    displayPNG: async function (imgContents) {
-        const buffer = await image.convertPNGto1BitBWRotated(imgContents);
-        this.driver.display(buffer);
-    },
-    init: function () {
-        this.driver.init();
-    },
 };
 
 const waveshare3in7Vertical = {
+    ...defaultDevice,
     height: 480,
     width: 280,
+    rotated: false,
+    rightToLeft: false,
+    numColors: 2,
     driver: waveshare3In7Driver,
-    displayPNG: async function (imgContents) {
-        const buffer = await image.convertPNGto1BitBW(imgContents);
-        this.driver.display(buffer);
-    },
-    init: function () {
-        this.driver.init();
-    },
 };
 
 const waveshare3in7Horizontal = {
+    ...defaultDevice,
     height: 280,
     width: 480,
+    rotated: true,
+    rightToLeft: false,
+    numColors: 2,
     driver: waveshare3In7Driver,
-    displayPNG: async function (imgContents) {
-        const buffer = await image.convertPNGto1BitBWRotated(imgContents);
-        this.driver.display(buffer);
-    },
-    init: function () {
-        this.driver.init();
-    },
 };
 
 const waveshare3in7VerticalGray = {
+    ...defaultDevice,
     height: 480,
     width: 280,
+    rotated: false,
+    rightToLeft: false,
+    numColors: 4,
     driver: waveshare3In7Driver,
-    displayPNG: async function (imgContents) {
-        const buffer = await image.convertPNGto4Grey(imgContents);
-        this.driver.display_4Gray(buffer);
-    },
-    init: function () {
-        this.driver.init_4Gray();
-    },
 };
 
 const waveshare3in7HorizontalGray = {
+    ...defaultDevice,
     height: 280,
     width: 480,
+    rotated: true,
+    rightToLeft: false,
+    numColors: 4,
     driver: waveshare3In7Driver,
-    displayPNG: async function (imgContents, color_depth) {
-        const buffer = await image.convertPNGto4GreyRotated(imgContents);
-        this.driver.display_4Gray(buffer);
-    },
-    init: function () {
-        this.driver.init_4Gray();
-    },
 };
 
 const waveshare2in13v2Vertical = {
+    ...defaultDevice,
     height: 250,
     width: 122,
+    rotated: false,
+    rightToLeft: true,
+    numColors: 2,
     driver: waveshare2in13v2Driver,
-    displayPNG: async function (imgContents) {
-        const buffer = await image.convertPNGto1BitBW2in13V2(imgContents);
-        this.driver.display(buffer);
-    },
-    init: function () {
-        this.driver.init();
-    },
 };
 
 const waveshare2in13v2Horizontal = {
+    ...defaultDevice,
     height: 122,
     width: 250,
+    rotated: true,
+    rightToLeft: true,
+    numColors: 2,
     driver: waveshare2in13v2Driver,
-    displayPNG: async function (imgContents) {
-        const buffer = await image.convertPNGto1BitBW2in13V2Rotated(
-            imgContents
-        );
-        this.driver.display(buffer);
-    },
-    init: function () {
-        this.driver.init();
-    },
 };
 
 const waveshare2in13bcHorizontal = {
+    ...defaultDevice,
     height: 104,
     width: 212,
+    rotated: true,
+    rightToLeft: false,
+    numColors: 2,
     driver: waveshare2in13bcDriver,
-    displayPNG: async function (imgContents) {
-        const blackBuffer = await image.convertPNGto1BitBWRotated(imgContents);
-        const emptyBuffer = Buffer.alloc(blackBuffer.length, 0xff);
-        this.driver.display(blackBuffer, emptyBuffer);
-    },
-    init: function () {
-        this.driver.init();
-    },
 };
 
 const waveshare2in13bcVertical = {
+    ...defaultDevice,
     height: 212,
     width: 104,
+    rotated: false,
+    rightToLeft: false,
+    numColors: 2,
     driver: waveshare2in13bcDriver,
-    displayPNG: async function (imgContents) {
-        const blackBuffer = await image.convertPNGto1BitBW(imgContents);
-        const emptyBuffer = Buffer.alloc(blackBuffer.length, 0xff);
-        this.driver.display(blackBuffer, emptyBuffer);
-    },
-    init: function () {
-        this.driver.init();
-    },
 };
 
 const waveshare2in7v2Vertical = {
+    ...defaultDevice,
     height: 176,
     width: 264,
+    rotated: false,
+    rightToLeft: true,
+    numColors: 2,
     driver: waveshare2in7v2Driver,
-    displayPNG: async function (imgContents) {
-        const buffer = await image.convertPNGto1BitBW2in13V2(imgContents);
-        this.driver.display(buffer);
-    },
-    init: function () {
-        this.driver.init();
-    },
 };
 
 const waveshare2in7Horizontal = {
+    ...defaultDevice,
     height: 176,
     width: 264,
+    rotated: true,
+    rightToLeft: false,
+    numColors: 2,
     driver: waveshare2in7Driver,
-    displayPNG: async function (imgContents) {
-        const buffer = await image.convertPNGto1BitBWRotated(imgContents);
-        this.driver.display(buffer);
-    },
-    init: function () {
-        this.driver.init();
-    },
 };
 const waveshare2in7Vertical = {
+    ...defaultDevice,
     height: 264,
     width: 176,
+    rotated: false,
+    rightToLeft: false,
+    numColors: 2,
     driver: waveshare2in7Driver,
-    displayPNG: async function (imgContents) {
-        const buffer = await image.convertPNGto1BitBW(imgContents);
-        this.driver.display(buffer);
-    },
-    init: function () {
-        this.driver.init();
-    },
 };
 
 const waveshare2in7v2Horizontal = {
+    ...defaultDevice,
     height: 264,
     width: 176,
+    rotated: true,
+    rightToLeft: true,
+    numColors: 2,
     driver: waveshare2in7v2Driver,
-    displayPNG: async function (imgContents) {
-        const buffer = await image.convertPNGto1BitBW2in13V2Rotated(
-            imgContents
-        );
-        this.driver.display(buffer);
-    },
-    init: function () {
-        this.driver.init();
-    },
 };
 
 const waveshare2in7bHorizontal = {
+    ...defaultDevice,
     height: 264,
     width: 176,
+    rotated: true,
+    rightToLeft: false,
+    numColors: 2,
     driver: waveshare2in7bDriver,
-    displayPNG: async function (imgContents) {
-        const blackBuffer = await image.convertPNGto1BitBWRotated(imgContents);
-        const emptyBuffer = Buffer.alloc(blackBuffer.length, 0xff);
-        this.driver.display(blackBuffer, emptyBuffer);
-    },
-    init: function () {
-        this.driver.init();
-    },
 };
 
 const waveshare2in7bVertical = {
+    ...defaultDevice,
     height: 176,
     width: 264,
+    rotated: false,
+    rightToLeft: false,
+    numColors: 2,
     driver: waveshare2in7bDriver,
-    displayPNG: async function (imgContents) {
-        const blackBuffer = await image.convertPNGto1BitBW(imgContents);
-        const emptyBuffer = Buffer.alloc(blackBuffer.length, 0xff);
-        this.driver.display(blackBuffer, emptyBuffer);
-    },
-    init: function () {
-        this.driver.init();
-    },
 };
 const waveshare2in7V2bHorizontal = {
+    ...defaultDevice,
     height: 264,
     width: 176,
+    rotated: true,
+    rightToLeft: false,
+    numColors: 2,
     driver: waveshare2in7v2bDriver,
-    displayPNG: async function (imgContents) {
-        const blackBuffer = await image.convertPNGto1BitBWRotated(imgContents);
-        const emptyBuffer = Buffer.alloc(blackBuffer.length, 0xff);
-        this.driver.display(blackBuffer, emptyBuffer);
-    },
-    init: function () {
-        this.driver.init();
-    },
 };
 
 const waveshare2in7V2bVertical = {
+    ...defaultDevice,
     height: 176,
     width: 264,
+    rotated: false,
+    rightToLeft: false,
+    numColors: 2,
     driver: waveshare2in7v2bDriver,
-    displayPNG: async function (imgContents) {
-        const blackBuffer = await image.convertPNGto1BitBW(imgContents);
-        const emptyBuffer = Buffer.alloc(blackBuffer.length, 0xff);
-        this.driver.display(blackBuffer, emptyBuffer);
-    },
-    init: function () {
-        this.driver.init();
-    },
 };
 
 const devices = {
@@ -313,7 +264,8 @@ const devices = {
     // default waveshare7in5v2 kept for backwards compatibility with releaes 1.1.0
     waveshare7in5v2: waveshare7in5v2Horizontal,
     waveshare7in5v2Horizontal,
-    waveshare7in2v2Vertical,
+    waveshare7in5v2Vertical,
+    waveshare7in2v2Vertical: waveshare7in5v2Vertical, // backwards compatible typo
     // default
     waveshare3in7: waveshare3in7HorizontalGray,
     waveshare3in7Horizontal,
--- image.js
+++ image.js
@@ -8,7 +8,12 @@ const allocBuffer_8 = (devWidth, devHeight) =>
 const allocBuffer_4 = (devWidth, devHeight) =>
     Buffer.alloc(Math.ceil(devWidth / 4) * devHeight, 0xff);
 
-function convertPNGto1BitBW(pngBytes) {
+function convertPNGto1BitBW(
+    pngBytes,
+    rotated = false,
+    dither = false,
+    rightToLeft = false // used for 2in13V2
+) {
     const reader = new PNGReader(pngBytes);
     return new Promise((resolve, reject) => {
         reader.parse((err, png) => {
@@ -17,95 +22,58 @@ function convertPNGto1BitBW(pngBytes) {
             }
             const height = png.getHeight();
             const width = png.getWidth();
-            const outBuffer = allocBuffer_8(width, height);
+            const devHeight = rotated ? width : height;
+            const devWidth = rotated ? height : width;
+            const outBuffer = allocBuffer_8(devWidth, devHeight);
 
-            const quants = (new Array(width).fill(1).map(() => new Array(height).fill(0)));
+            // store of quantization errors (for dithering)
+            const quants =
+                dither &&
+                new Array(width).fill(1).map(() => new Array(height).fill(0));
             const storeQuantErr = (x, y, v) => {
                 if (x < 0 || x >= width || y >= height) return;
-                quants[x][y] += v
-            }
+                quants[x][y] += v;
+            };
 
-            for (let y = 0; y < height; y++) {
-                for (let x = 0; x < width; x++) {
-                    const [r, g, b] = png.getPixel(x, y);
-                    const luma = getLuma(r, g, b);
-
-
-                    const oldValue = luma + quants[x][y];
-                    const value = Math.round(oldValue / 255) * 255;
-                    const qu_err = oldValue - value;
-
-                    storeQuantErr(x+1, y  , qu_err * 7/16);
-                    storeQuantErr(x-1, y+1, qu_err * 3/16);
-                    storeQuantErr(x  , y+1, qu_err * 5/16);
-                    storeQuantErr(x+1, y+1, qu_err * 1/16);
-
-                    if (value < 128) {
-                        const out_index = Math.floor((x + y * width) / 8);
-                        outBuffer[out_index] &= ~(0x80 >> Math.floor(x % 8));
-                    }
-                }
-            }
-            resolve(outBuffer);
-        });
-    });
-}
-
-function convertPNGto1BitBW2in13V2(pngBytes) {
-    const reader = new PNGReader(pngBytes);
-    return new Promise((resolve, reject) => {
-        reader.parse((err, png) => {
-            if (err) {
-                return reject(err);
-            }
-            const height = png.getHeight();
-            const width = png.getWidth();
-            const lineWidth =
-                width % 8 === 0
-                    ? Math.floor(width / 8)
-                    : Math.floor(width / 8) + 1;
-            const outBuffer = allocBuffer_8(width, height);
-            for (let y = 0; y < height; y++) {
-                for (let x = 0; x < width; x++) {
-                    const [r, g, b, alpha] = png.getPixel(x, y);
-                    const luma = getLuma(r, g, b);
-                    const outX = width - x;
-                    if (luma < 50) {
-                        const out_index = Math.floor(outX / 8) + y * lineWidth;
-                        outBuffer[out_index] &= ~(0x80 >> outX % 8);
-                    }
-                }
-            }
-            resolve(outBuffer);
-        });
-    });
-}
-
-function convertPNGto1BitBW2in13V2Rotated(pngBytes) {
-    const reader = new PNGReader(pngBytes);
-    return new Promise((resolve, reject) => {
-        reader.parse((err, png) => {
-            if (err) {
-                return reject(err);
-            }
-            const height = png.getHeight();
-            const width = png.getWidth();
-            const devHeight = width;
-            const devWidth = height;
+            // used for rightToLeft
             const lineWidth =
                 devWidth % 8 === 0
                     ? Math.floor(devWidth / 8)
                     : Math.floor(devWidth / 8) + 1;
-            const outBuffer = allocBuffer_8(devWidth, devHeight);
+
             for (let y = 0; y < height; y++) {
                 for (let x = 0; x < width; x++) {
-                    const outX = y;
-                    const outY = width - (devHeight - x - 1) - 1;
-                    const [r, g, b, alpha] = png.getPixel(x, y);
-                    const luma = getLuma(r, g, b);
+                    let outX = rotated ? y : x;
+                    let outY = rotated ? devHeight - x - 1 : y;
+
+                    if (rightToLeft) {
+                        outX = rotated ? y : devWidth - x;
+                        outY = rotated
+                            ? devHeight - (devHeight - x - 1) - 1
+                            : y;
+                    }
+
+                    const [r, g, b] = png.getPixel(x, y);
+                    let luma = getLuma(r, g, b);
+
+                    if (dither) {
+                        const oldValue = luma + quants[x][y];
+                        const value = Math.round(oldValue / 255) * 255;
+                        const qu_err = oldValue - value;
+
+                        storeQuantErr(x + 1, y, (qu_err * 7) / 16);
+                        storeQuantErr(x - 1, y + 1, (qu_err * 3) / 16);
+                        storeQuantErr(x, y + 1, (qu_err * 5) / 16);
+                        storeQuantErr(x + 1, y + 1, (qu_err * 1) / 16);
+
+                        luma = value;
+                    }
+
                     if (luma < 50) {
-                        const out_index = Math.floor(outX / 8) + outY * lineWidth;
-                        outBuffer[out_index] &= ~(0x80 >> y % 8);
+                        const out_index = rightToLeft
+                            ? Math.floor(outX / 8) + y * lineWidth
+                            : Math.floor((outX + outY * devWidth) / 8);
+                        outBuffer[out_index] &= ~(0x80 >> Math.floor(outX % 8));
                     }
                 }
             }
@@ -114,36 +82,7 @@ function convertPNGto1BitBW2in13V2Rotated(pngBytes) {
     });
 }
 
-function convertPNGto1BitBWRotated(pngBytes) {
-    const reader = new PNGReader(pngBytes);
-    return new Promise((resolve, reject) => {
-        reader.parse((err, png) => {
-            if (err) {
-                return reject(err);
-            }
-            const height = png.getHeight();
-            const width = png.getWidth();
-            const devHeight = width;
-            const devWidth = height;
-            const outBuffer = allocBuffer_8(devWidth, devHeight);
-            for (let y = 0; y < height; y++) {
-                for (let x = 0; x < width; x++) {
-                    const outX = y;
-                    const outY = devHeight - x - 1;
-                    const [r, g, b, alpha] = png.getPixel(x, y);
-                    const luma = getLuma(r, g, b);
-                    if (luma < 50) {
-                        const out_index = Math.floor((outX + outY * devWidth) / 8);
-                        outBuffer[out_index] &= ~(0x80 >> Math.floor(y % 8));
-                    }
-                }
-            }
-            resolve(outBuffer);
-        });
-    });
-}
-
-async function convertPNGto4Grey(pngBytes) {
+async function convertPNGto4Grey(pngBytes, rotated = false) {
     const pngBytes_L = await sharp(pngBytes).greyscale().png().toBuffer();
     const reader = new PNGReader(pngBytes_L);
     return new Promise((resolve, reject) => {
@@ -153,54 +92,30 @@ async function convertPNGto4Grey(pngBytes) {
             }
             const height = png.getHeight();
             const width = png.getWidth();
-            const outBuffer = allocBuffer_4(width, height);
-            let i = 0;
-            for (let y = 0; y < height; y++) {
-                for (let x = 0; x < width; x++) {
-                    if (++i % 4 == 0) {
-                        const out_index = Math.floor((x + y * width) / 4);
-                        outBuffer[out_index] =
-                            (getGrayPixel(png.getPixel(x - 3, y)) & 0xc0) |
-                            ((getGrayPixel(png.getPixel(x - 2, y)) & 0xc0) >>
-                                2) |
-                            ((getGrayPixel(png.getPixel(x - 1, y)) & 0xc0) >>
-                                4) |
-                            ((getGrayPixel(png.getPixel(x, y)) & 0xc0) >> 6);
-                    }
-                }
-            }
-            resolve(outBuffer);
-        });
-    });
-}
-
-async function convertPNGto4GreyRotated(pngBytes) {
-    const pngBytes_L = await sharp(pngBytes).greyscale().png().toBuffer();
-    const reader = new PNGReader(pngBytes_L);
-    return new Promise((resolve, reject) => {
-        reader.parse((err, png) => {
-            if (err) {
-                return reject(err);
-            }
-            const height = png.getHeight();
-            const width = png.getWidth();
-            const devHeight = width;
-            const devWidth = height;
+            const devHeight = rotated ? width : height;
+            const devWidth = rotated ? height : width;
             const outBuffer = allocBuffer_4(devWidth, devHeight);
             let i = 0;
-            for (let x = 0; x < width; x++) {
-                for (let y = 0; y < height; y++) {
-                    const outX = y;
-                    const outY = devHeight - x - 1;
+            for (let a = 0; a < devHeight; a++) {
+                for (let b = 0; b < devWidth; b++) {
+                    const x = rotated ? a : b;
+                    const y = rotated ? b : a;
+
+                    const outX = rotated ? y : x;
+                    const outY = rotated ? devHeight - x - 1 : y;
                     if (++i % 4 == 0) {
-                        const out_index = Math.floor((outX + outY * devWidth) / 4);
+                        const out_index = Math.floor(
+                            (outX + outY * devWidth) / 4
+                        );
+                        const pixel = (diff) =>
+                            rotated
+                                ? png.getPixel(x, y - diff)
+                                : png.getPixel(x - diff, y);
                         outBuffer[out_index] =
-                            (getGrayPixel(png.getPixel(x, y - 3)) & 0xc0) |
-                            ((getGrayPixel(png.getPixel(x, y - 2)) & 0xc0) >>
-                                2) |
-                            ((getGrayPixel(png.getPixel(x, y - 1)) & 0xc0) >>
-                                4) |
-                            ((getGrayPixel(png.getPixel(x, y)) & 0xc0) >> 6);
+                            (getGrayPixel(pixel(3)) & 0xc0) |
+                            ((getGrayPixel(pixel(2)) & 0xc0) >> 2) |
+                            ((getGrayPixel(pixel(1)) & 0xc0) >> 4) |
+                            ((getGrayPixel(pixel(0)) & 0xc0) >> 6);
                     }
                 }
             }
@@ -222,9 +137,5 @@ function getGrayPixel(rgba) {
 
 module.exports = {
     convertPNGto1BitBW,
-    convertPNGto1BitBWRotated,
     convertPNGto4Grey,
-    convertPNGto4GreyRotated,
-    convertPNGto1BitBW2in13V2,
-    convertPNGto1BitBW2in13V2Rotated,
 };
